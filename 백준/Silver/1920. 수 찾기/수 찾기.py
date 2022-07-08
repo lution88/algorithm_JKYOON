5cd5945e1a7a8a2ecadbd216c99a3f715ba71d3b
JKYOON_88 <78908697+lution88@users.noreply.github.com>
2022-07-08 15:39:42 +0900

    [Silver IV] Title: 수 찾기, Time: 212 ms, Memory: 51400 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,30 +1,16 @@
-num_n = int(input())
-group_a = sorted(map(int, input().split()))
+import sys
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
 
-num_m = int(input())
-group_b = list(map(int, input().split()))
+m = int(sys.stdin.readline())
+b = list(map(int, sys.stdin.readline().split()))
 
+key_hash = {}
+for key in a:
+    key_hash[key] = True
 
-def binary_search(arr, target, start, end):
-    if start > end:
-        return None
-    # 중간점 설정
-    mid = (start + end) // 2
-    # 찾은 경우 중간점 인덱스 변환
-    if arr[mid] == target:
-        return mid
-    # 찾는 값이 중간점보다 작은경우
-    elif arr[mid] > target:
-        return binary_search(arr, target, start, mid-1)
-    # 찾는 값이 중간점보다 큰 경우
-    else:
-        return binary_search(arr, target, mid+1, end)
-
-
-for i in group_b:
-    start, end = 0, num_n-1
-    result = binary_search(group_a, i, start, end)
-    if result == None:
-        print(0)
-    else:
+for key in b:
+    if key_hash.get(key):
         print(1)
+    else:
+        print(0)
