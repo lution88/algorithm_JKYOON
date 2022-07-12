05608b23928d9eda25cd09e8d62593c93cba86a2
JKYOON_88 <78908697+lution88@users.noreply.github.com>
2022-07-12 15:39:26 +0900

    [level 2] Title: 더 맵게, Time: 2126.72 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,18 @@
 import heapq
 
-def solution(scoville, K):
-    heapq.heapify(scoville)
-    answer = 0
 
-    while len(scoville) >= 2:
-        min_1 = heapq.heappop(scoville)
-        
-        if min_1 >= K:
+def solution(scoville, K):
+    heapq.heapify(scoville) # heapq 선언
+    answer = 0
+    scoville_index = 1
+
+    while scoville_index <= len(scoville)-1:
+        min1 = heapq.heappop(scoville) # 가장 작은 원소를 pop
+        if min1 >= K:
             return answer
         else:
-            min_2 = heapq.heappop(scoville)
-            heapq.heappush(scoville, min_1 + (min_2 * 2))
+            min2 = heapq.heappop(scoville) # 두 번째 작은 원소를 pop
+            heapq.heappush(scoville, min1 + (min2 * 2)) # 스코빌 지수 섞어서 push
             answer += 1
 
     if scoville[0] > K:
