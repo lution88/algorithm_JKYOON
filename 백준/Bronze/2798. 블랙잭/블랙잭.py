3f574a57c4096ade4839a6100636edaac779bdf3
JKYOON_88 <78908697+lution88@users.noreply.github.com>
2022-08-10 10:57:03 +0900

    [Bronze II] Title: 블랙잭, Time: 164 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -7,9 +7,7 @@ total = 0
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
-            if card_numbers[i] + card_numbers[j] + card_numbers[k] > M:
-                continue
-            else:
+            if card_numbers[i] + card_numbers[j] + card_numbers[k] <= M:
                 total = max(total, card_numbers[i] + card_numbers[j] + card_numbers[k])
 
 print(total)
